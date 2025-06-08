a5d54091c684d62c649ab2241d1c073c400d3478
Graham Nickbel <134581805+grahamnickbel23@users.noreply.github.com>
2025-06-08 01:46:38 +0000

    amazing animation

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener("pageshow", function (event) {
 });
 
 function replayAnimations() {
-    document.querySelectorAll(".premium-pannel-content , .more-premium-pannel-content")
+    document.querySelectorAll(".header, .landing-page , .landing-page-heading , .landing-page-sub-heading, .premium-pannel-content , .more-premium-pannel-content")
         .forEach((el) => {
             el.style.animation = "none";
             el.offsetHeight; // trigger reflow
